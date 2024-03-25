b332bd433a9bf8ef27767f2a20fa6c75c041deed
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-25 03:40:38 +0200

    output issue

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,7 @@
 #configuration file to can connect to a server without typing a password.
-Host *
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
+file { 'Config-file':
+  ensure  => 'file',
+  path    => '/etc/ssh/ssh_config',
+  content => '  IdentityFile ~/.ssh/school
+                PasswordAuthentication no',
+}
