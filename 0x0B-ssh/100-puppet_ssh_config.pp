2c0d1ae2ea1ce00e22281e3f4b71dbf436ca18ec
sabah-hue <sabah.abdelbaset@gmail.com>
2024-04-20 02:33:44 +0200

    delete directory 0x0A 0x0B

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,7 +0,0 @@
-#configuration file to can connect to a server without typing a password.
-file { 'Config-file':
-  ensure  => 'file',
-  path    => '/etc/ssh/ssh_config',
-  content => '  IdentityFile ~/.ssh/school
-                PasswordAuthentication no',
-}
