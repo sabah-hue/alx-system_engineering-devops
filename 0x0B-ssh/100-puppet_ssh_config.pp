86c7a80c6bcebb8f414bf2eb24c7418cbc7aee77
sabah-hue <sabah.abdelbaset@gmail.com>
2024-04-22 04:24:50 +0200

    ssh config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+# SSH configuration file for the local SSH client
+file { 'ssh_config':
+    ensure    => 'file',
+    path   => '~/etc/ssh/ssh_config',
+    content   => 'Host *
+                      IdentityFile ~/.ssh/school
+                      PasswordAuthentication no',
+}
