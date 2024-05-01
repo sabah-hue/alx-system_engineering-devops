6bdb9e7a59eb8763b17c73159eac7ade55c2db32
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-01 15:16:55 +0300

    output issue

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,7 +2,6 @@
 exec { 'install_nginx':
     command  => 'sudo apt-get -y update;
     sudo apt-get -y install nginx;
-    sudo ufw allow 'Nginx HTTP';
     sudo chown -R "$USER":"$USER" /var/www/html/index.nginx-debian.html;
     sudo echo "Hello World!" > /var/www/html/index.nginx-debian.html;
     sudo sed -i "/server_name _;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default;
