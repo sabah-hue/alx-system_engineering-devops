56f9cc05f73a8d57ed982401dc3cd84e4bb53d85
sabah-hue <sabah.abdelbaset@gmail.com>
2024-06-05 02:42:55 +0300

    req issue

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,14 @@ function that queries the Reddit API and returns the number of subscribers
 """
 
 import requests
+import threading
 
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, allow_redirects=False,
-                            headers={"User-Agent": "My-User-Agent"})
-    if response.status_code <= 300:
+    response = requests.get(url, timeout=10, allow_redirects=False)
+    if response.status_code == 200:
         data = response.json()
         count = data['data']['subscribers']
     else:
