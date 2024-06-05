1af1579f4b06df6911050d7856dcc3294f83da2c
sabah-hue <sabah.abdelbaset@gmail.com>
2024-06-05 05:49:47 +0300

    check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,24 +8,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
-    # url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    # response = requests.get(url, timeout=10, allow_redirects=False)
-    # if response.status_code == 200:
-    #     data = response.json()
-    #     count = data['data']['subscribers']
-    # else:
-    #     count = 0
-    # return count
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, timeout=10, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        count = data['data']['subscribers']
+    else:
+        count = 0
+    return count
