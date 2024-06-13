32947fada407b97ecf2f1f309e3397b4b9c02399
sabah-hue <sabah.abdelbaset@gmail.com>
2024-06-13 05:27:07 +0000

    add user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,3 @@
 #Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.
 
-exec { 'add-user':
-    command => 'sudo adduser holberton',
-    path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-    onlyif  => 'id -u holberton >/dev/null 2>&1 ||! id -u holberton',
-}
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
