10127b3f326be02ed1b2f4ca6920b85b1b98f03a
sabah-hue <sabah.abdelbaset@gmail.com>
2024-06-13 05:00:40 +0000

    debug

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
 # bring ULIMIT higher
 exec { 'higher-ulimit':
-    command => '/usr/local/bin:/bin/sed -i "s/15/2000" /etc/default/nginx',
+    command => 'sed -i "s/15/2000" /etc/default/nginx',
+    path    => '/usr/local/bin/:/bin/',
 }
 
 exec { 'restat-nginx':
