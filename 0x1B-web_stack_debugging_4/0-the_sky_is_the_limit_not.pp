6eb2a86d997f2ac6a4f1f93856a82c4d93399416
sabah-hue <sabah.abdelbaset@gmail.com>
2024-06-13 05:12:39 +0000

    debug

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,11 @@
 # bring ULIMIT higher
-exec { 'higher-ulimit':
-    command => 'sed -i "s/15/2000" /etc/default/nginx',
-    path    => '/usr/local/bin/:/bin/',
+file { 'nginx_ulimit_settings':
+  ensure  => file,
+  path    => '/etc/default/nginx',
+  content => "# Note: You may want to look at the following page before setting the ULIMIT.\n# http://wiki.nginx.org/CoreModule#worker_rlimit_nofile\n# Set the ulimit variable if you need defaults to change.\n# Example: ULIMIT=\"-n 4096\"\nULIMIT=\"-n 4096\"",
 }
 
 exec { 'restat-nginx':
-    command => '/etc/init.d/nginx restart',
+    command => 'nginx restart',
+    path    => '/etc/init.d/'
 }
